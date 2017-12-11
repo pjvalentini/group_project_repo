7dc09cfcd3635ca202a84e138c24ccdd099a2a2e
pjvalentini <pj.valentini@gmail.com>
2017-12-11 17:56:53 -0500

    still fixing connections

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -1,23 +1,62 @@
 /* global $ */
+
 $(document).ready(function() {
-	$('#calendar').fullCalendar({
-		header: {
-			left: '',
-			center: 'title',
-			right: 'today prev, next',
-		},
-		dayClick: function(date, jsEvent, view) {
-    	console.log('Clicked on: ' + date.format());
-			console.log('Current view: ' + view.name);
-        // change the day's background color just for fun
-			$(this).css('background-color:hover', 'red');
-			var infoId = $(this).data('id');
-			$.ajax({
-				method: 'GET', // getting quotes from DB
-				url: '/api/calendar',
-			}).then(function(info) {
-				console.log(info);
-			});
-		},
+	$('#sign-in-form').on('submit', function(e) {
+		e.preventDefault();
+
+		var signInObj = {
+			username: $('#username-input').val(),
+			password: $('#password-input').val(),
+		};
+
+		$.ajax({
+			method: 'POST',
+			url: '/api/sign-in',
+			dataType: 'json',
+			data: JSON.stringify(signInObj),
+			contentType: 'application/json',
+		}).then(function(res) {
+			window.location.href = '/api/profile/' + res.results[0].id;
+		});
+
+		$('#name-input').val("");
+		$('#username-input').val("");
+	});
+
+
+	$.ajax({
+		method: 'GET',
+		url: '/api/calendar',
+	}).then((res) => {
+		console.log(res);
+	    $('#calendar').fullCalendar({
+				schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+	    	// defaultView: 'agendaDay',
+		    events: res,
+		    resources: [
+		        { id: 'a', title: 'Room A' },
+		        { id: 'b', title: 'Room B' },
+		        { id: 'c', title: 'Room C' },
+		        { id: 'd', title: 'Room D' },
+		    ],
+		    dayClick: function(date, jsEvent, view) {
+		        console.log('Clicked on: ' + date.format());
+		        console.log('jsEvent: ' + jsEvent);
+
+		       	console.log(view);
+		       console.log(jsEvent);
+
+		        $(this).css('background-color', 'red');
+		       	console.log($(this));
+		    },
+		    eventClick: function(event, element) {
+		    	console.log(event.title);
+		        event.title = "CLICKED!";
+		        $('#calendar').fullCalendar('updateEvent', event);
+		    },
+	    });
+
+	    // var arr = $('#calendar').fullCalendar('clientEvents');
+	    // console.log(arr)
 	});
 });
--- server/controller/routes.js
+++ server/controller/routes.js
@@ -1,44 +1,44 @@
-var pg = require('pg');
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var pg = require('pg');
 
-var dbUrl;
+var localCon = {
+	user: process.argv.POSTGRES_USER,
+	password: process.argv.POSTGRES_PASSWORD,
+	database: 'world_holidays',
+	host: 'localhost',
+	port: 5432,
+};
 
-if (process.env.DATABASE_URL) {
-	dbUrl = process.env.DATABASE_URL;
-} else {
-	dbUrl = {
-		user: process.argv.POSTGRES_USER,
-		password: process.argv.POSTGRES_PASSWORD,
-		database: 'world_holidays',
-		host: 'localhost',
-		port: 5432,
-	};
-}
+let dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL : localCon;
 
 var pgClient = new pg.Client(dbUrl);
+
 pgClient.connect();
 
-var html_creator = require('../helpers/html_creator.js');
 var router = express.Router();
 
-// first basic route for index.html page.
-router.get('/', function(req, res) {
+router.get('/', function(req,res){
 	res.sendFile(path.join(__dirname, '../../client/public/html/index.html'));
 });
 
-router.get('/api/calendar', (req, res) => {
-	var info = 'SELECT * FROM world_holidays';
-	pgClient.query(info, (error, getInfo) => {
-		console.log(getInfo);
-		if (error) {
-			res.json(error);
-		} else {
-			res.json(getInfo);
+router.get('/api/calendar', function(req,res) {
+	pgClient.query("SELECT * FROM holidays", (err,results) => {
+		var dec = [];
+		for (var i = 0; i < results.rows.length; i++) {
+		 if (results.rows[i].month === "December") {
+			let day = results.rows[i].day.length > 1 ? results.rows[i].day : `0${results.rows[i].day}`;
+			var data = {};
+			data.title = results.rows[i].info;
+			data.start = "2017-12-" + day;
+			data.editable = true;
+			data.eventStartEditable = true;
+			dec.push(data);
+		 }
 		}
- 	});
+		res.json(dec);
+	});
 });
-
-// Other routes to go here!
 
 module.exports = router;
