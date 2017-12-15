9443dad9bf184955ad4e61c7715866f86106659a
pjvalentini <pj.valentini@gmail.com>
2017-12-14 19:29:46 -0500

    Removed some duplicate code

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -23,19 +23,6 @@ $(document).ready(function() {
 		$('#username-input').val("");
 	});
 
-			var infoId = $(this).data('id');
-
-			$.ajax({
-				method: 'GET', // getting quotes from DB
-				url: '/api/calendar',
-			}).then(function(info) {
-				// console.log(info);
-				for (var i = 0; i < info.rows.length; i++) {
-					if (info.rows[i].id === infoId) {
-						// console.log(infoId);
-					}
-				}
-			});
 
 	function monthConvertor(month) {
 		switch (month) {
